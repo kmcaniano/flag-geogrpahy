92b0ac249d60336512c9b3649ca4aed7e0eea0e3
Kevin Caniano <kmcaniano@gmail.com>
2022-07-17 18:41:52 -0500

    Refactor folders

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(path.join(__dirname, '..', publicPath)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
