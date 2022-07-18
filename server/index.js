7c9b523ad14f435d4438501069ba26cf5037262a
Kevin Caniano <kmcaniano@gmail.com>
2022-07-17 20:47:42 -0500

    Node change

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use('/flags', express.static(__dirname + '/public/flags'));
-
 app.use('/', express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
