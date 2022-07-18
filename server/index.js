95772db6d4c8b7d5c7090521be7751bd55867978
Kevin Caniano <kmcaniano@gmail.com>
2022-07-17 22:05:42 -0500

    More changes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/flags',express.static(path.join(__dirname, 'public/flags')));
+app.use('/topoJson',express.static(path.join(__dirname, 'public/topoJson')));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
