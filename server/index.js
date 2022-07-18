b28aea581aa9003791ea56e577c6185322511672
Kevin Caniano <kmcaniano@gmail.com>
2022-07-17 19:54:29 -0500

    More changes

--- server/index.js
+++ server/index.js
@@ -5,12 +5,9 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('public'));
-app.use('/flags', express.static('public/flags'));
+app.use('/flags', express.static(__dirname + '/public/flags'));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
