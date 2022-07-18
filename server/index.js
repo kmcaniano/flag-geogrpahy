7b371e9a92abd0b8cc1775ae57d8fb90e5517e37
Kevin Caniano <kmcaniano@gmail.com>
2022-07-17 21:35:11 -0500

    More Express

--- server/index.js
+++ server/index.js
@@ -5,8 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.static('public'))
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
