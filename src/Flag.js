048e5d31d4711b504ca23ecbcdfd681868ebd3d9
Kevin Caniano <kmcaniano@gmail.com>
2022-07-17 20:43:18 -0500

    Update image paths

--- src/Flag.js
+++ src/Flag.js
@@ -3,10 +3,10 @@ import Container from '@mui/material/Container';
 
 export default function Flag(props) {
 
-  if (props.countryCode != '') {
+  if (props.countryCode !== '') {
     return <Container>
       <Box component="img" sx={{
         width: "200px", height: "100px", display: "block", margin: "0 auto"
-      }} src={process.env.PUBLIC_URL + '/flags/' + props.countryCode + '.png'} /></Container>
+      }} src={'./flags/' + props.countryCode + '.png'} /></Container>
   }
 }
